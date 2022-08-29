7a441d3a1e4155ab327b0b733068f6d8ca180e5c
s4saikat87 <saikatg@fi-tek.co.in>
2022-08-29 15:53:14 +0530

    reporting

---
+++ src/components/acctHoldingGrid.js
@@ -0,0 +1,225 @@
+import React from 'react'
+import { useState, useEffect, useRef } from 'react';
+import { Grid, GridColumn as Column, GridToolbar } from "@progress/kendo-react-grid";
+import { process } from "@progress/kendo-data-query";
+import { ExcelExport } from '@progress/kendo-react-excel-export';
+
+const aggregates = [
+  {
+    field: "cost",
+    aggregate: "sum",
+  },
+  {
+    field: "market",
+    aggregate: "sum",
+  },
+  {
+    field: "unrGainLoss",
+    aggregate: "sum",
+  },
+  {
+    field: "principalCash",
+    aggregate: "sum",
+  },
+  {
+    field: "incomeCash",
+    aggregate: "sum",
+  },
+  {
+    field: "investedIncome",
+    aggregate: "sum",
+  }
+
+];
+const initialGroup = [
+  {
+    field: "branch",
+  }
+
+];
+const AcctHoldingGrid = ({data}) => {
+  const [locked, setLocked] = React.useState(false);
+
+  const handleClick = () => {
+    setLocked(!locked);
+  };
+  const _export = React.useRef(null);
+
+    const excelExport = () => {
+    if (_export.current !== null) {
+        _export.current.save(resultState);
+    }
+  };
+
+  const totalSum = (props) => {
+    const field = props.field || "";
+    const total = data.reduce((acc, current) => acc + current[field], 0).toFixed(2);
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+         {total}
+      </td>
+    );
+  };
+  const initialDataState = {};
+  const [row, setRow] = useState(data);
+  const [dataState, setDataState] = React.useState();
+  const [resultState, setResultState] = React.useState(
+    process(row, initialDataState)
+  );
+  //setResultState(process({data}, initialDataState))
+  let total = row.length;
+  let pageSize = 20;
+  const [page, setPage] = React.useState({
+    skip: 0,
+    take: pageSize,
+  });
+  const onDataStateChange = React.useCallback((e) => {
+   debugger;
+    setDataState(e.dataState);
+    //let gridData = data;
+    const groups = e.dataState.group;
+
+    if (groups) {
+      groups.map((group) => (group.aggregates = aggregates));
+    }
+    e.dataState.group = groups;
+   setResultState( process(row,e.dataState));
+  }, []);
+
+  
+
+  
+
+  const cellRender = (tdElement, cellProps) => {
+
+    if (cellProps.rowType === "groupFooter") {
+
+      if (cellProps.field === "cost") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+             {cellProps.dataItem.aggregates.cost.sum.toFixed(2)}
+          </td>
+        );
+      }
+      if (cellProps.field === "market") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+             {cellProps.dataItem.aggregates.market.sum.toFixed(2)}
+          </td>
+        );
+      }
+      if (cellProps.field === "unrGainLoss") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+             {cellProps.dataItem.aggregates.unrGainLoss.sum.toFixed(2)}
+          </td>
+        );
+      }
+      if (cellProps.field === "principalCash") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+             {cellProps.dataItem.aggregates.unrGainLoss.sum.toFixed(2)}
+          </td>
+        );
+      }
+      if (cellProps.field === "incomeCash") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+             {cellProps.dataItem.aggregates.unrGainLoss.sum.toFixed(2)}
+          </td>
+        );
+      }
+      if (cellProps.field === "investedIncome") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+             {cellProps.dataItem.aggregates.unrGainLoss.sum.toFixed(2)}
+          </td>
+        );
+      }
+    }
+
+    return tdElement;
+  };
+   
+  return (
+    
+    <div>
+        {
+        
+        <div className='card-header row d-flex justify-content-between align-items-center my-2'>
+
+          <div className='col'>
+            <p className='tableheader h6'>Account Holding Report</p>
+            </div>
+            <div className='col'></div>
+          
+          <div className='col'>
+          <button className='btn btn-outline-primary btn-sm' onClick={excelExport}>Export to Excel</button>
+
+            </div>
+
+
+        </div>
+        /* <div className="card mx-2 my-2">
+            <div className="card-header tableheader">Account Transaction Report</div>
+        </div> */}
+        <div className="card-body">
+        <div className="mx-1 my-1 py-1">
+        <ExcelExport data={resultState} ref={_export}> 
+       <Grid style={{ height: "600px" }}
+            data={resultState}
+       
+            groupable={{
+              footer: "visible",
+            }}
+            sortable={true}
+           // pageSize={pageSize}
+           // total={total}
+           // filterable={true}
+           onDataStateChange={onDataStateChange}
+           {...dataState}
+
+            cellRender={cellRender}
+          >
+            
+            <Column field="branch" menu={true} title="Branch" width="150px" locked={true} />
+            <Column field="accountType" menu={true} title="Account Type" width="150px"  locked={true}/>
+            <Column field="accountName" menu={true} title="Account Name" width="150px" locked={true} />
+            <Column field="asset" menu={true} title="Asset Description" width="150px" />
+            <Column field="tckrSymbl" menu={true} title="Ticker" width="150px" />
+            <Column field="cusip" menu={true} title="Cusip" width="150px" />
+            <Column field="pmrDesc" menu={true} title="PMR" width="150px" />
+            <Column field="shares" title="Shares" format="{0:n2}" width="150px" filter="numeric" filterable={false} />
+            <Column field="cost" title="Cost" width="150px" footerCell={totalSum} format="{0:n2}" filter="numeric" filterable={false}/>
+            <Column field="market" title="Market" width="150px" footerCell={totalSum} format="{0:n2}" filter="numeric" filterable={false}/>
+
+            <Column field="unrGainLoss" title="Unr Gain Loss" footerCell={totalSum} format="{0:n2}" width="150px" filter="numeric" filterable={false}/>
+            <Column field="estAnnInc" title="Est Ann Inc"format="{0:n2}" width="150px" filter="numeric" filterable={false}/>
+            <Column field="yield" title="Yield" width="150px" format="{0:n2}" filter="numeric" filterable={false}/>
+            <Column field="accruedInterest" title="Acc Int" format="{0:n2}" width="150px" filter="numeric" filterable={false}/>
+            <Column field="principalCash" title="PCash" width="150px" footerCell={totalSum} format="{0:n2}" filter="numeric" filterable={false}/>
+            <Column field="incomeCash" title="ICash" width="150px" footerCell={totalSum} format="{0:n2}" filter="numeric" filterable={false}/>
+            <Column field="investedIncome" title="Invested Income" footerCell={totalSum} format="{0:n2}" width="150px" filter="numeric" filterable={false}/>
+            <Column field="investmentObjective" title="Inv Objective" width="150px" filter="numeric" filterable={false}/>
+            <Column field="administrator" title="Administrator" width="150px" />
+            <Column field="investmentOfficer" title="Inv Officer" width="150px" />
+            <Column field="rate" title="Rate" format="{0:n2}" filter="numeric" filterable={false} width="150px" />
+            <Column field="txCstAmt" title="Tax Cost" format="{0:n2}" filter="numeric" filterable={false} width="150px" />
+            <Column field="yldToCost" title="Yeild To Cost" format="{0:n2}" filter="numeric" filterable={false} width="150px" />
+
+           
+          </Grid>
+          </ExcelExport> 
+          </div>
+          </div>
+    </div>
+  )
+}
+
+export default AcctHoldingGrid
---
+++ src/components/fixdIncmFndmntlsGrid.js
@@ -0,0 +1,260 @@
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Grid, GridColumn as Column, GridToolbar } from "@progress/kendo-react-grid";
+import { process } from "@progress/kendo-data-query";
+import { ExcelExport } from '@progress/kendo-react-excel-export';
+import Moment from 'react-moment';
+import { formatNumber, formatDate  } from '@telerik/kendo-intl';
+
+const aggregates = [
+    {
+      field: "shares",
+      aggregate: "sum",
+    },
+    {
+        field: "market",
+        aggregate: "sum",
+    },
+    {
+        field: "yield",
+        aggregate: "average",
+    }
+  
+  ];
+  const initialGroup = [
+    {
+      field: "mtrtyYr",
+    }  
+  ];
+const FixdIncmFundmntlsGrid = ({data}) => {
+    debugger;
+    const _export = React.useRef(null);
+
+    const excelExport = () => {
+    if (_export.current !== null) {
+        _export.current.save(resultState);
+    }
+  };
+
+  const totalSum = (props) => {
+    const field = props.field || "";
+    const total = data.reduce((acc, current) => acc + current[field], 0);
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+       { formatNumber(total, "##,#.00")}
+        
+      </td>
+    );
+  };
+  const avgYield = (props) => {
+    debugger;
+    const field = props.field;
+    const len=data.length;
+    const average = data.reduce((acc, current) => acc + current[field],0)/len;
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+         { formatNumber(average, "##,#.00")}
+      </td>
+    );
+  }; 
+
+  const initialDataState = {};
+  const [row, setRow] = useState(data);
+  const [dataState, setDataState] = React.useState();
+  const [resultState, setResultState] = React.useState(
+    process(row, initialDataState)
+  );
+  //setResultState(process({data}, initialDataState))
+  let total = row.length;
+  let pageSize = 10;
+  const [page, setPage] = React.useState({
+    skip: 0,
+    take: pageSize
+  });
+  const onDataStateChange = React.useCallback((e) => {
+   debugger;
+    
+    //let gridData = data;
+    const groups = e.dataState.group;
+
+    if (groups) {
+      groups.map((group) => (group.aggregates = aggregates));
+    }
+    e.dataState.group = groups;
+   setResultState( process(row,e.dataState));
+   setDataState(e.dataState);
+  }, []);
+
+  const cellRender = (tdElement, cellProps) => {
+    {/*if (cellProps.rowType === "footer") {
+      debugger;
+      if (cellProps.field === "Shares") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+            Sum: {cellProps.dataItem.aggregates.Shares.sum}
+          </td>
+        );
+      }
+      if (cellProps.field === "Market") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+            Sum: {cellProps.dataItem.aggregates.Market.sum}
+          </td>
+        );
+      }
+    }*/}
+    
+    if (cellProps.rowType === "data")
+    {
+    let cpnRate="", matrtyDate="";
+    
+    if(cellProps.field==="maturityDt")
+    {
+      
+      const value = cellProps.field;{/* this.props.dataItem[this.props.field];*/}      
+      return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+            {/*  {formatDate(new Date( cellProps.dataItem[value]), "d")}*/}
+              <Moment format="MM/DD/YYYY">
+              {cellProps.dataItem[value]}
+            </Moment>
+          </td>
+      );
+    }
+    if(cellProps.field==="astShrtNm")
+    {
+      cpnRate=cellProps.dataItem["couponRate"];
+      matrtyDate=cellProps.dataItem["maturityDt"];
+      return (
+        <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+          {cellProps.dataItem[cellProps.field]}&nbsp;{ formatNumber(cpnRate, "##,#.00")}%&nbsp;
+            <Moment format="MM/DD/YYYY">
+            {matrtyDate}
+          </Moment>
+          &nbsp;
+        </td>
+    );
+    }
+  }
+
+    if (cellProps.rowType === "groupFooter") {
+
+      if (cellProps.field === "shares") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+            {cellProps.dataItem.aggregates.shares.sum}
+          </td>
+        );
+      }
+      if (cellProps.field === "market") {
+
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+            {cellProps.dataItem.aggregates.market.sum}
+          </td>
+        );
+      }
+      if (cellProps.field === "yield") {
+debugger
+        return (
+          <td aria-colindex={cellProps.columnIndex} role={"gridcell"}>
+            { formatNumber(cellProps.dataItem.aggregates.yield.average, "##,#.00")}
+          </td>
+        );
+      }
+    }
+
+    return tdElement;
+  };
+  return (
+    
+    <div>
+        <div className="card mx-2 my-2">
+            <div className="card-header tableheader">Fixed Income Fundamentals Report</div>
+        </div>
+        <div className="container-fluid">
+        <div className="row text-center">
+        <ExcelExport data={resultState} ref={_export}> 
+       <Grid style={{ height: "650px" }}
+            data={resultState}
+       
+            groupable={{
+              footer: "visible",
+            }}
+            sortable={true}
+            pageable={{pageSize:true}}
+            //pageSize={pageSize}
+           // total={total}
+           // filterable={true}
+           onDataStateChange={onDataStateChange}
+           {...dataState}
+
+            cellRender={cellRender}
+          >
+            <GridToolbar>
+          <button
+            title="Export Excel"
+            className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary"
+            onClick={excelExport}
+          >
+            Export to Excel
+          </button>
+        </GridToolbar>
+            <Column field="mtrtyYr" menu={true} title="Maturity Year" width="150px"  />
+            {/*<Column field="couponRate" menu={true} title="Coupon Rate" width="150px" />
+            <Column field="maturityDt"  menu={true}  filter="date" title="Maturity Date" width="150px" />*/}
+            <Column field="astShrtNm" menu={true}  title="Description" width="300px" />
+
+
+
+           {/* <Column field="BranchName" menu={true} title="Branch" width="150px" />
+            <Column field="AccountType" menu={true} title="Account Type" width="150px" />
+            <Column field="AccountName" menu={true} title="Account Name" width="150px" />
+             <Column field="prcsDt" menu={true} title="Trans. Date" width="150px" />
+            <Column field="tranTypNm" menu={true} title="Trans. Type" width="150px" />
+            
+            <Column field="totalLine" menu={true} title="Description" width="300px" />
+            <Column field="administrator" title="Administrator" width="150px" />
+        <Column field="investmentOfficer" title="Inv Officer" width="150px" />*/}
+            
+
+            <Column field="shares" title="Shares" width="150px" filter="numeric" format="{0:n2}" footerCell={totalSum} filterable={false}/>
+            <Column field="market" title="Market($)" width="150px" format="{0:n2}" filter="numeric" footerCell={totalSum} filterable={false}/>
+            <Column field="yield" title="Yield%" width="150px" filter="numeric" format="{0:n2}" footerCell={avgYield}  filterable={false} />
+
+            <Column field="moodyRating" menu={true} title="Moody Rating" width="150px" />
+            <Column field="sPRating" menu={true} title="SP Rating" width="150px" />
+            
+
+            {/* <Column
+            field="PriceHistory"
+            title="Price history"
+            cell={SparkLineChartCell}
+          /> */}
+            {/* <Column
+            field="Discontinued"
+            width="130px"
+            cell={props => (
+              <td>
+                <input
+                  className="k-checkbox"
+                  type="checkbox"
+                  disabled
+                  defaultChecked={props.dataItem[props.field]}
+                />
+                <label className="k-checkbox-label" />
+              </td>
+            )}
+          /> */}
+          </Grid>
+          </ExcelExport>   
+          </div>
+          </div>
+    </div>
+  )
+}
+
+export default FixdIncmFundmntlsGrid
